8cfffb00ddb86d59cc132db132b0e51dcfce33d3
jamin <bennyfils@gmail.com>
2026-05-23 00:19:25 +0100

    oacage-lock.json

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
+require("dotenv").config();
+
+const cors = require("cors");
 const express = require("express");
 const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 5500;
+const corsOrigin = process.env.CORS_ORIGIN || "*";
 
+app.use(cors({ origin: corsOrigin }));
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
-
-const PORT = 5500;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
